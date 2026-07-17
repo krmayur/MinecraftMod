51aadadfc3e3758e7adc198d049d4cdab0263d4a
krmayur <mayuresh936@gmail.com>
2026-07-18 00:13:50 +0530

    moon added

--- src/main/java/com/mayuresh/portableweathermachine/item/PortableWeatherMachineItem.java
+++ src/main/java/com/mayuresh/portableweathermachine/item/PortableWeatherMachineItem.java
@@ -17,41 +17,72 @@ public class PortableWeatherMachineItem extends Item {
     public InteractionResult use(Level level, Player player, InteractionHand hand) {
 
         if (!level.isClientSide()) {
-            long time = level.getDayTime() % 24000;
-            String weather;
-            long dayNumber = (level.getDayTime() / 24000) + 1;
 
-            if (level.isThundering()) {
-                weather = "Thunderstorm";
-            } else if (level.isRaining()) {
-                weather = "Rain";
-            } else {
-                weather = "Clear";
-            }
+            String weather = getWeather(level);
+            long dayNumber = getDayNumber(level);
+            String timeOfDay = getTimeOfDay(level);
+            String moonPhase = getMoonPhase(level);
 
-            String timeOfDay;
-
-            if (time < 1000) {
-                timeOfDay = "Sunrise";
-            } else if (time < 6000) {
-                timeOfDay = "Morning";
-            } else if (time < 12000) {
-                timeOfDay = "Afternoon";
-            } else if (time < 13000) {
-                timeOfDay = "Sunset";
-            } else if (time < 18000) {
-                timeOfDay = "Evening";
-            } else {
-                timeOfDay = "Night";
-            }
             player.displayClientMessage(
                     Component.literal("Weather: " + weather +
                                     " | Time: " + timeOfDay +
-                                    " | Day: " + dayNumber),
+                                    " | Day: " + dayNumber +
+                                    " | Moon: " + moonPhase
+                    ),
                     false
             );
         }
 
         return InteractionResult.SUCCESS;
+    }
+
+    private String getWeather(Level level) {
+        if (level.isThundering()) {
+            return "Thunderstorm";
+        } else if (level.isRaining()) {
+            return "Rain";
+        } else {
+            return "Clear";
+        }
+    }
+
+    private String getTimeOfDay(Level level) {
+        long time = level.getDayTime() % 24000;
+
+        if (time < 1000) {
+            return "Sunrise";
+        } else if (time < 6000) {
+            return "Morning";
+        } else if (time < 12000) {
+            return "Afternoon";
+        } else if (time < 13000) {
+            return "Sunset";
+        } else if (time < 18000) {
+            return "Evening";
+        } else {
+            return "Night";
+        }
+    }
+
+    private long getDayNumber(Level level) {
+        return (level.getDayTime() / 24000) + 1;
+    }
+
+    private String getMoonPhase(Level level) {
+
+        long worldDay = level.getDayTime() / 24000L;
+        int phase = (int) (worldDay % 8);
+
+        return switch (phase) {
+            case 0 -> "Full Moon";
+            case 1 -> "Waning Gibbous";
+            case 2 -> "Last Quarter";
+            case 3 -> "Waning Crescent";
+            case 4 -> "New Moon";
+            case 5 -> "Waxing Crescent";
+            case 6 -> "First Quarter";
+            case 7 -> "Waxing Gibbous";
+            default -> "Unknown";
+        };
     }
 }
